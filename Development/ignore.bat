c9d0a76b9c001a3ce87e010ed70c69261081a62b
dev_sl <you@example.com>
2020-04-10 14:46:11 +0800

    change ico to ksj company ico and add ignore.bat delete surplus program file

---
+++ Development/ignore.bat
@@ -0,0 +1,11 @@
+cd Samples\VC
+for /d %%i in (*) do ( 
+cd %%i
+rd x86 x64 ipch /s /q
+del *.sdf *.suo *.log *.user *.filters *.ipch *.aps /s
+cd..)
+cd..
+
+cd C#\KSJShow3D_CSharp
+rd obj /s /q
+del *.suo /s
